f38f5b52d96648e7e5fa4cacb8b0c9cc25e7af2a
alexiaeg11 <147570369+alexiaeg11@users.noreply.github.com>
2024-04-02 18:30:42 -0600

    app_y_home

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import React from 'react';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import './App.css'
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Perfil from './views/Perfil';
+import Home from './views/Home';
 import Inicio from './views/Inicio';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 function App() {
-  return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Inicio />} />
 
+  return (  
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='' element={<Inicio />} />
+          <Route path='productos' element={"<Productos />"} />
+          <Route path='favoritos' element={"<Favoritos />"} />
+          <Route path='carrito' element={"<Carrito />"} />
+        </Route>
+
+        <Route path='/logearse' element={''} />
+        <Route path='/registrarse' element={''} />
+        <Route path='/perfil' element={<Perfil />} />
+        <Route path='/admin' element={"<LoginAdmin />"} />
+        <Route path='/dashboard' element={"<LoginAdmin />"} />
+        
       </Routes>
-      <Footer />
     </HashRouter>
-  );
+
+  )
 }
 
-export default App;
+export default App
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { BrowserRouter} from 'react-router-dom'
-import App from '../App'
+import Carousel from '../components/Carousel'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 export default function Home() {
     return (
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <>
+            <Navbar />
+
+            <Outlet />
+
+            <Footer />
+        </>
     )
 }
