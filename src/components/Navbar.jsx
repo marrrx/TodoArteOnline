91cde0d30450bd910895e3e5a934229307d2cf29
alexiaeg11 <147570369+alexiaeg11@users.noreply.github.com>
2024-04-03 08:13:24 -0600

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,28 +18,28 @@ export default function Navbar() {
             <nav className="navbar" style={{ backgroundColor: 'black', height: 150 }} data-bs-theme='dark'>
                 <div className="container-fluid text-center">
                     <div className="row w-100">
-                        <div className='col-2'>
+                        <div className='col-6'>
+
                             <a className="navbar-brand" onClick={handleHome}><img src="https://raw.githubusercontent.com/marrrx/Imagenes/main/logo.png" style={{ width: 100, height: 100 }} /></a>
                         </div>
-                        <div className='col-6 align-content-center'>
-                            <form className="d-flex" role="search">
-                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                            </form>
-                        </div>
-                        <div className='col-3 align-content-center'>
-                            <div className={`navbar-toggler ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-                                <span></span>
-                                <span></span>
-                                <span></span>
+
+                        <div className='col-6 text-end'>
+                            <button className="navbar-toggler" onClick={toggleMenu}>
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className='col-6 align-content-center'>
+                                <form className="d-flex" role="search">
+                                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                    <button className="btn btn-outline-success" type="submit">Search</button>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                {menuOpen && (
-                    <div className="menu-right">
-                        <ul className="nav text">
+                <div className={`navbar-collapse justify-content-end ${menuOpen ? 'show' : ''}`} style={{ backgroundColor: menuOpen ? 'black' : 'transparent', width: '200px', height: 'calc(100vh - 150px)', position: 'fixed', top: '150px', right: 0, zIndex: 1 }}>
+                    {menuOpen && (
+                        <ul className="nav flex-column text text-end">
                             <li className="nav-item">
                                 <Link to={'productos'}>
                                     <a className="nav-link active" aria-current="page" onClick={toggleMenu}>Productos</a>
@@ -62,12 +62,12 @@ export default function Navbar() {
                             </li>
                             <li className="nav-item">
                                 <Link to={'signin'}>
-                                        <a className="nav-link active" aria-current="page" onClick={toggleMenu}>Registrarse</a>
+                                    <a className="nav-link active" aria-current="page" onClick={toggleMenu}>Registrarse</a>
                                 </Link>
                             </li>
                         </ul>
-                    </div>
-                )}
+                    )}
+                </div>
             </nav>
         </>
     );
