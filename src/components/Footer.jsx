f59b3c8d04cef5a4eb2c1d3b2b958477fa88df0c
Daniel-taco <cervantes.romo.oscar@gmai.com>
2024-04-08 19:48:30 -0600

    fix_facebook

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,7 @@ export default function Footer() {
               <h5>Contactos</h5>
               <ul className='list-unstyled'>
               <li><a href='#'>Comunícate con nosotros</a></li>
-                <li>info@aguascalientes.tecnm.mx</li>
+                <li>info@aguascalientes.tecnm.mxddd</li>
                 <li>+52 465 113 4563</li>
                 <li>610 Dionsio Mtz. Trejo, Aguascalientes, México.</li>
               </ul>
@@ -33,7 +33,7 @@ export default function Footer() {
             <div className="col-sm-3">
               <h5>Síguenos en redes sociales</h5>
               <ul className="list-unstyled d-flex">
-                <Link to="https://www.facebook.com/profile.php?id=61558073106514t">
+                <Link to="https://www.facebook.com/profile.php?id=61558073106514&mibextid=LQQJ4d">
                 <li className="me-3"><a><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
                 </svg></a></li>
