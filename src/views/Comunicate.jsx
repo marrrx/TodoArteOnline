f60bc1e04c9bac6dc1ce2f8d2680267fb7fb3337
melanielpez11 <melanielpezmtz@gmail.com>
2024-05-19 15:46:54 -0600

    datos requeridos en email

--- src/views/Comunicate.jsx
+++ src/views/Comunicate.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-
-import { useNavigate } from 'react-router-dom'
-
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Comunicate() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleHome = () => {
-  event.preventDefault();
-  alert('Su mensaje fue enviado con éxito.');
-  navigate('/');
-};
+  const handleHome = (event) => {
+    event.preventDefault();
+    // Verificar si todos los campos obligatorios están completos
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const asunto = document.getElementById('asunto').value;
+    const comentarios = document.getElementById('comentarios').value;
+
+    if (nombre && email && asunto && comentarios) {
+      alert('Su mensaje fue enviado con éxito.');
+      navigate('/');
+    } else {
+      alert('Por favor complete todos los campos son obligatorios.');
+    }
+  };
 
   return (
     <>
@@ -21,27 +28,27 @@ const handleHome = () => {
           <form>
             <div className="form-group">
               <label htmlFor="nombre">Nombre</label>
-              <input type="text" className="form-control" name="name" id="nombre" required />
+              <input type="text" className="form-control" id="nombre" required />
             </div>
             <div className="form-group">
               <label htmlFor="email">Correo electrónico</label>
-              <input type="email" className="form-control" name="email" id="email" required />
+              <input type="email" className="form-control" id="email" required />
             </div>
             <div className="form-group">
               <label htmlFor="asunto">Asunto</label>
-              <input type="text" className="form-control" name="subject" id="asunto" required />
+              <input type="text" className="form-control" id="asunto" required />
             </div>
             <div className="form-group">
               <label htmlFor="comentarios">Comentarios</label>
-              <textarea className="form-control" name="comments" id="comentarios" cols="15" rows="5" required></textarea>
+              <textarea className="form-control" id="comentarios" cols="15" rows="5" required></textarea>
             </div>
             <div>
-              <label>Una vez enviado tu mensaje, te redirigiramos a la página de incio</label>
+              <label>Una vez enviado tu mensaje, te redirigiremos a la página de inicio</label>
             </div>
             <button onClick={handleHome} className="btn btn-primary">Enviar</button>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
